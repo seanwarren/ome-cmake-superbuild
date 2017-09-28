2a2449698985550ec45601593e6e522acd134849
Sean Warren <s.warren@garvan.org.au>
2017-09-28 20:16:07 +1000

    Make xalan and xerces link test a warning - currently broken on windows

--- packages/ome-files/superbuild.cmake
+++ packages/ome-files/superbuild.cmake
@@ -13,7 +13,7 @@ ome_source_settings(ome-files
 ome_add_dependencies(ome-files
                      DEPENDENCIES ome-model
                      THIRD_PARTY_DEPENDENCIES boost png tiff
-                                              py-sphinx gtest)
+                                              py-sphinx gtest patch)
 
 list(APPEND CONFIGURE_OPTIONS
      "-DBOOST_ROOT=${OME_EP_INSTALL_DIR}"
@@ -30,6 +30,13 @@ ExternalProject_Add(${EP_PROJECT}
   SOURCE_DIR ${EP_SOURCE_DIR}
   BINARY_DIR ${EP_BINARY_DIR}
   INSTALL_DIR ""
+  PATCH_COMMAND
+  ${CMAKE_COMMAND}
+  "-DSOURCE_DIR:PATH=${EP_SOURCE_DIR}"
+  "-DPATCH_DIR:PATH=${CMAKE_CURRENT_LIST_DIR}/patches"
+  "-DCONFIG:INTERNAL=$<CONFIG>"
+  "-DEP_SCRIPT_CONFIG:FILEPATH=${EP_SCRIPT_CONFIG}"
+  -P "${GENERIC_PATCH}"
   CONFIGURE_COMMAND ${CMAKE_COMMAND}
     "-DSOURCE_DIR:PATH=${EP_SOURCE_DIR}"
     "-DBUILD_DIR:PATH=${EP_BINARY_DIR}"
